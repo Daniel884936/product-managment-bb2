60ef186dbeefcb535bea340d9e29b879b1ab187c
Daniel <tejadadaniel33@gmail.com>
2021-07-05 21:45:41 -0400

    springfox dependecy added

--- src/main/java/com/productmanagment/productmanagment/queryFilters/ProductQueryFilter.java
+++ src/main/java/com/productmanagment/productmanagment/queryFilters/ProductQueryFilter.java
@@ -1,4 +1,7 @@
 package com.productmanagment.productmanagment.queryFilters;
 
+import com.productmanagment.productmanagment.models.ProductState;
+
 public class ProductQueryFilter {
+    ProductState productState;
 }
